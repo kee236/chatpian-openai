236f9a72c1e30fede04948fe89506253d5663548
kee236 <31869356+kee236@users.noreply.github.com>
2025-01-28 15:56:30 +0700

    Update Tiktok_model.php

--- application/modules/tiktok/models/Tiktok_model.php
+++ application/modules/tiktok/models/Tiktok_model.php
@@ -20,3 +20,37 @@ class Tiktok_model extends CI_Model
         $this->db->insert("tiktok_accounts", $data);
     }
 }
+
+
+public function get_account_by_id($account_id)
+{
+    return $this->db->get_where('tiktok_accounts', ['id' => $account_id])->row_array();
+}
+
+public function save_auto_reply($user_id, $video_id, $reply_text)
+{
+    $data = [
+        'user_id' => $user_id,
+        'video_id' => $video_id,
+        'reply_text' => $reply_text,
+        'created_at' => date('Y-m-d H:i:s')
+    ];
+    $this->db->insert('tiktok_auto_replies', $data);
+}
+
+
+
+public function get_analytics_data($user_id)
+{
+    return $this->db->get_where('tiktok_analytics', ['user_id' => $user_id])->result_array();
+}
+
+public function get_user_accounts($user_id)
+{
+    return $this->db->get_where('tiktok_accounts', ['user_id' => $user_id])->result_array();
+}
+
+public function delete_account($account_id)
+{
+    $this->db->delete('tiktok_accounts', ['id' => $account_id]);
+}
